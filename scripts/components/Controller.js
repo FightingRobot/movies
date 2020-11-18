17ca75f544cd95aebd9839970316f1d6723494f8
Pavel Khatyliou <hatylevpavel@gmail.com>
2020-11-18 21:15:46 +0300

    feat: main functionality finished

--- scripts/components/Controller.js
+++ scripts/components/Controller.js
@@ -1,19 +1,39 @@
 import { DATA_LINK } from '../helpers/constants.js';
+import { createCard } from '../helpers/functions.js';
 
 class Controller {
-  constructor() {
-    this.value = 0;
+  getData = () => {
+    return fetch(DATA_LINK)
+      .then(responce => responce.json())
+      .then(json => json.data)
   }
 
-  getData = async () => {
-    const responce = await fetch(DATA_LINK);
-    const json = await responce.json();
-    return json.data;
+  getImage = (obj) => {
+    return fetch(obj.poster_path)
+      .then(img => img.blob())
+      .then(function (blob) {
+        if (blob.type === 'image/jpeg') {
+          return URL.createObjectURL(blob);
+        }
+        throw Error();
+      })
+      .catch(error => '../../assets/placeholder.png');
   }
 
-  start = async () => {
-    let data = await this.getData();
-    console.log(data)
+  insertCards = (data) => {
+    const content = document.querySelector('.movie-list');
+
+    for (let obj of data) {
+      this.getImage(obj)
+        .then(img => content.append(createCard(obj, img)))
+        .catch(error => console.log(error));;
+    }
+  }
+
+  start = () => {
+    this.getData()
+      .then(data => this.insertCards(data))
+      .catch(error => console.log(error));
   }
 }
 
