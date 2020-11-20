2886b2ef2b47c5ec40e24f3b8c17bddceb657a84
Pavel Khatyliou <hatylevpavel@gmail.com>
2020-11-20 12:14:21 +0300

    feat: add functions

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
+
+// import { DATA_LINK } from './helpers/constants.js';
+// import { getData, getImage, createCard } from './helpers/functions.js';
+
 import './main.scss';
 
 function App() {
 
+
   return (
     <div className="movies">
-      <header>
-        <h1>Movies</h1>
+      <div className="loader">
+        <div className="loader__circle"></div>
+      </div>
+      <header className="header">
+        <div className="content header__content">
+          <h1>MOVIES (ES6)</h1>
+        </div>
       </header>
-      <main>
-
+      <main className="main">
+        <div className="content main__content">
+          <div className="movie-list">
+          </div>
+        </div>
       </main>
     </div>
   );
