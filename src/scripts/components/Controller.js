9dd300f8953ab241b73ab19d6822215c85131176
Pavel Khatyliou <hatylevpavel@gmail.com>
2020-11-29 17:16:06 +0300

    refactor: massieve refactoring

--- src/scripts/components/Controller.js
+++ src/scripts/components/Controller.js
@@ -6,26 +6,42 @@ class Controller {
     this.movieList = document.querySelector('.movie-list');
     this.loader = document.querySelector('.loader');
     this.placeholder = placeholder;
+
+    this.data = [];
+    this.images = [];
+  }
+
+  loadImages(data) {
+    let promises = data.map((a) => getImage(a, this.placeholder));
+    return Promise.all(promises);
+  }
+
+  generateCards(data, img) {
+    const cards = [];
+    data.map((item, index) => { cards.push(createCard(item, img[index])) });
+    return cards;
   }
 
   toggleLoader() {
     this.loader.classList.toggle('loader_disable')
   }
 
-  insertCards(data) {
-    for (let obj of data) {
-      getImage(obj, this.placeholder)
-        .then(img => this.movieList.append(createCard(obj, img)))
-        .catch(error => console.log(error));;
-    }
+  insertCards(cards) {
+    cards.map(a => this.movieList.append(a))
   }
 
   start() {
     getData(DATA_LINK)
-      .then(data => this.insertCards(data))
-      .then(() => this.toggleLoader())
-      .catch(error => console.log(error));
+      .then(data => {
+        this.data = data;
+        return this.loadImages(data)
+          .then(images => this.images = images)
+      })
+      .then(() => this.generateCards(this.data, this.images))
+      .then((cards) => this.insertCards(cards))
+      .catch(error => this.movieList.append(error))
+      .finally(() => this.toggleLoader());
   }
 }
 
-export default Controller;
+export default Controller
