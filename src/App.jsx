4685bd1389282f84cbea3cecc36ec3d6bde5493d
Pavel Khatyliou <hatylevpavel@gmail.com>
2020-12-01 19:24:57 +0300

    feat: get rid of controller component & minor refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { DATA_LINK, MAX_ITEMS } from './helpers/constants';
+import { getData, getImage } from './helpers/functions.js';
 
 import Header from './components/Header/Header';
-import Controller from './components/Controller/Controller';
+import CreateCard from './components/CreateCard/CreateCard.jsx';
+import Loader from './components/Loader/Loader.jsx';
 
 import './main.scss';
 
 function App() {
 
+  const [data, setData] = useState([]);
+  const [images, setImages] = useState([]);
+
+  const loadImages = async (data) => {
+    const imgArr = [];
+
+    for (let i = 0; i < data.length; i++) {
+      const img = await getImage(data[i]);
+      imgArr.push(img);
+    }
+
+    return imgArr;
+  }
+
+  const generateCards = (data, images) => {
+    const cards = [];
+
+    for (let i = 0; i < data.length; i++) {
+      cards.push(<CreateCard object={data[i]} image={images[i]} key={i} />)
+    }
+
+    return cards;
+  }
+
+  useEffect(async () => {
+    const fetchData = await getData(DATA_LINK)
+    const fetchImages = await loadImages(fetchData);
+
+    setData(fetchData);
+    setImages(fetchImages);
+  }, [])
+
   return (
-    <div className="movies">
-      <Header />
-      <Controller />
-    </div>
+    images.length === MAX_ITEMS ? (
+      <div className="movies">
+        <Header />
+        <main className="main">
+          <div className="content main__content">
+            <div className="movie-list">
+              {generateCards(data, images)}
+            </div>
+          </div>
+        </main>
+      </div>
+    ) : (
+        <Loader />
+      )
   );
 }
 
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,20 +1,10 @@
 import React from 'react';
 
-import { MAX_ITEMS } from '../../helpers/constants.js'
-
 import './Loader.scss';
 
 const Loader = (props) => {
-  const toggleLoader = (images) => {
-    const classes = ['loader'];
-    if (images.length === MAX_ITEMS) {
-      classes.push('loader_disable')
-    }
-    return classes.join(' ');
-  }
-
   return (
-    <div className={toggleLoader(props.images)}>
+    <div className="loader">
       <div className="loader__circle"></div>
     </div>
   )
