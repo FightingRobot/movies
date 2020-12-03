17f51013329961894bf39a938854135c8773d5f4
Pavel Khatyliou <hatylevpavel@gmail.com>
2020-12-03 15:01:14 +0300

    feat: add search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import { DATA_LINK, MAX_ITEMS } from './helpers/constants';
 import { getData, getImage } from './helpers/functions.js';
@@ -11,8 +11,10 @@ import './main.scss';
 
 function App() {
 
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(new Array(1));
   const [images, setImages] = useState([]);
+  const [error, setError] = useState('');
+  const inputRef = useRef();
 
   const loadImages = async (data) => {
     const imgArr = [];
@@ -35,21 +37,57 @@ function App() {
     return cards;
   }
 
-  useEffect(async () => {
-    const fetchData = await getData(DATA_LINK)
-    const fetchImages = await loadImages(fetchData);
-
+  const loadData = async (link) => {
+    const fetchData = await getData(link)
     setData(fetchData);
+
+    if (!fetchData.length) {
+      setError('Nothing is found')
+    }
+
+    const fetchImages = await loadImages(fetchData);
     setImages(fetchImages);
+  }
+
+  const submitForm = () => {
+    event.preventDefault();
+
+    const requestBody = inputRef.current.value;
+
+    if (!requestBody.length) {
+      setError('Your request is empty')
+      return;
+    }
+
+    setData(new Array(1));
+    setImages([]);
+    setError('');
+
+    return loadData(`${DATA_LINK}?searchBy=title&search=${requestBody}&offset=0`);
+  }
+
+  useEffect(async () => {
+    try {
+      await loadData(DATA_LINK);
+    } catch {
+      setError('Failed to fetch data! Check your network settings.')
+    }
   }, [])
 
   return (
-    images.length === MAX_ITEMS ? (
+    images.length === data.length ? (
       <div className="movies">
         <Header />
         <main className="main">
           <div className="content main__content">
             <div className="movie-list">
+              <div className="movie-list__searchbar">
+                <form onSubmit={submitForm} action="" className="movie-list__searchform">
+                  <input ref={inputRef} placeholder="Find your movie" type="text" className="movie-list__searchinput" />
+                  <button className="movie-list__searchbtn"></button>
+                </form>
+                <p className='errorBox'>{error}</p>
+              </div>
               {generateCards(data, images)}
             </div>
           </div>
